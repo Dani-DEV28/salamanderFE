d0476b2b7a28be9edb6b0a166750666b3e9db48d
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-11-06 20:44:56 -0800

    fix the CSS for PSC file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-// import { useState } from 'react'
 import ProcessorStartCard from './components/ProcessorStartCard'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import './styles/salamander.css'
-// import './App.css'
 
 function App() {
-
   return (
-    <>
-      <Header/>
-      <div>
-        <ProcessorStartCard/>
-      </div>
-      <div>
-        <p>PlaceHolder</p>
-      </div>
-
-      <Footer/>
-    </>
-
+    <div className="app-container">
+      <Header />
+      <main className="app-content">
+        <ProcessorStartCard />
+        <p>Placeholder</p>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
 export default App
+
--- src/components/ProcessorStartCard.jsx
+++ src/components/ProcessorStartCard.jsx
@@ -1,18 +1,23 @@
-import salamanderImg from '../../public/images/salamander.jpg'
+import salamanderImg from '../assets/salamander.jpg'
 
 export default function ProcessorStartCard(){
     return(
         <div className="container-card-starter">
-            <div className="card-left">
-                <ol>
+            <div className="card-row">
+                <div className="card-left">
+                <ul>
                     <li>Import Video</li>
                     <li>Color</li>
                     <li>Threshold</li>
-                </ol>
-                <button type="submit"></button>
+                </ul>
+                </div>
+                <div className="card-right">
+                    <img src={salamanderImg} alt="Binarized Salamander Miku" />
+                </div>
             </div>
-            <div className="card-right">
-                <img src={salamanderImg} alt="Binarized Salamander Miku" />
+
+            <div className="button-lower">
+                <button type="submit">START</button>
             </div>
         </div>
     )
